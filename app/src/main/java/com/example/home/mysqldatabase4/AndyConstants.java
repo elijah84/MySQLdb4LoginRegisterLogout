23b66adeb82e2cfc8cd1eb7e448dfcba7dc3156c
elijah84 <assefa.belaye@gmail.com>
2017-10-11 16:52:44 -0700

    Initial commit

---
+++ app/src/main/java/com/example/home/mysqldatabase4/AndyConstants.java
@@ -0,0 +1,19 @@
+package com.example.home.mysqldatabase4;
+
+public class AndyConstants {
+    // web service url constants
+    public class ServiceType {
+        public static final String BASE_URL = "http://r3mm1k5.net/database4/";
+        public static final String LOGIN = BASE_URL + "simplelogin.php";
+        public static final String REGISTER =  BASE_URL + "simpleregister.php";
+
+    }
+    // webservice key constants
+    public class Params {
+
+        public static final String NAME = "name";
+        public static final String HOBBY = "hobby";
+        public static final String USERNAME = "username";
+        public static final String PASSWORD = "password";
+    }
+}
